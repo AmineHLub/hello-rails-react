1676416ec41d69d6032ecbfc9f3733157f0fe542
AmineHLub <Amino.bhl@gmail.com>
2022-02-16 00:09:08 +0100

    Finish getMessages async function and its dispatch

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,26 +1,32 @@
 import { createStore, applyMiddleware } from "redux";
+import Axios from 'axios';
 import thunk from 'redux-thunk';
 import Logger from 'redux-logger';
 const initialState = 'test';
 
 const GET_MSG_API = 'GET_MSG_API'
 
-export const getMessages = () => {
-  return{
-    type: GET_MSG_API
-  } 
+let baseUrl = 'v1/greetings.json'
+
+export const getMessages = () => async (dispatch) => {
+  const response = await Axios.get(baseUrl);
+  let messageObj = {message: response.data.greeting}
+  dispatch({
+    type: GET_MSG_API,
+    payload: messageObj,
+  });
 }
 
 function rootReducer(state= initialState, action) {
   switch (action.type) {
     case GET_MSG_API:
-    return 'Xello';
+    return action.payload;
     default: 
     return state;
   }
 }
 
 export default function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware(thunk, Logger));
+  const store = createStore(rootReducer,initialState, applyMiddleware(thunk, Logger));
   return store;
 }
