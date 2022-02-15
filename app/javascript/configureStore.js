5bc306ced5e0b67e9605e1f91f9e2fc006af1ce7
AmineHLub <Amino.bhl@gmail.com>
2022-02-15 23:44:29 +0100

    add static get messages function reducer

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,15 +1,26 @@
-import { createStore } from "redux";
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import Logger from 'redux-logger';
 const initialState = 'test';
 
-function rootReducer(state, action) {
+const GET_MSG_API = 'GET_MSG_API'
+
+export const getMessages = () => {
+  return{
+    type: GET_MSG_API
+  } 
+}
+
+function rootReducer(state= initialState, action) {
   switch (action.type) {
+    case GET_MSG_API:
+    return 'Xello';
     default: 
     return state;
   }
 }
 
 export default function configureStore() {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, applyMiddleware(thunk, Logger));
   return store;
 }
